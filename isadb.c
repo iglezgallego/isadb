cabe83bc68df93bc2f2c2eb5a11d3f96b5df0d75
iglezgallego <118050905+iglezgallego@users.noreply.github.com>
2024-01-17 20:43:15 +0100

    Entrada y salida

--- isadb.c
+++ isadb.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+//Uso parámetros en la función
+int main(int argc,char *argv[]){
     //Escribir en un archivo de texto
     FILE *archivo;
-    archivo = fopen("basededatos.txt","w");
-    char *texto = "ISADB";
-    fputs(texto,archivo);
+    archivo = fopen("basededatos.txt","a");
+    //Quiero atrapar el primer elemento que hay en el argumento 
+    //escribo en consola con "" lo que quiero que se escriba en el archivo, que es el parámetro que guarda
+    char *texto = argv[1];
+    fputs(strcat(texto,"\n"),archivo);
     fclose(archivo);
     return 0;
 }
